d6a38b08c734c63ae97a56ac2989732b0c23088f
Lee Garam <agerio100@naver.com>
2024-02-08 15:33:25 +0900

    Basic setup for MySQL connection
    Established a default MySQL connection with MySQL Workbench

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+
+	// Replace the connection parameters with your MySQL server details
+	mysqlConnectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		os.Getenv("MYSQL_USERNAME"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_ACCESS_HOST"),
+		os.Getenv("MYSQL_ACCESS_PORT"),
+		os.Getenv("MYSQL_DATABASE_NAME"))
+	db, err := sql.Open("mysql", mysqlConnectionString)
+	if err != nil {
+		fmt.Println("Failed to connect to MySQL:", err)
+		return
+	}
+	defer db.Close()
+
+	// Test the connection
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Failed to ping MySQL:", err)
+		return
+	}
+
+	fmt.Println("Connected to MySQL server!")
+}
